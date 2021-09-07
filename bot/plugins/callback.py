96efe3f91f3678fcd0b65d7bf621d9f5c8f2d86f
Hurricane-Hari <87891571+Hurricane-Hari@users.noreply.github.com>
2021-09-07 18:54:28 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -96,6 +96,7 @@ async def cb_navg(bot, update: CallbackQuery):
     if not int(index_val) == 0:    
         temp_results.append([
             InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton('How To Download Movies From us', url="https://t.me/STARZONE_MOVIEREQUEST/11335"
         ])
     
     if show_invite and int(index_val) !=0 :
@@ -142,7 +143,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>🥰Found</i> <code>{leng}</code> <i>Results For Your Request⬇️:</i> <code>{query}</code>"
+    text=f"<i>🥰Found</i> <code>{leng}</code> <i>Results For Your Request⬇👇:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
